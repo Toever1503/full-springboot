bc1a9d649ef509fe07c80cc0d3ace7e40f662091
Toever1503 <Haunv1503@gmail.com>
2022-06-11 14:18:55 +0700

    update ip cors

--- src/main/java/com/config/BeanConfiguration.java
+++ src/main/java/com/config/BeanConfiguration.java
@@ -36,7 +36,7 @@ public class BeanConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://192.168.1.19:8081", "http://3.34.133.37")
+                        .allowedOrigins("http://192.168.1.19:8081", "http://54.180.159.175", "http://54.180.159.175:3000")
                         .allowedOriginPatterns("*.*.*.*:*")
                         .allowCredentials(true)
                         .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS");
