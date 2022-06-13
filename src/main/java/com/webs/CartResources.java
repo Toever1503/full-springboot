2e2e913fd766ae02029b06d87cf31263922917d1
Đinh Thế Vũ <thevu091193@gmail.com>
2022-06-13 14:25:27 +0700

    fix list item in cart
    Done

--- src/main/java/com/webs/CartResources.java
+++ src/main/java/com/webs/CartResources.java
@@ -8,6 +8,7 @@ import com.models.CartModel;
 import com.repositories.IOptionsRepository;
 import com.services.ICartService;
 import com.services.IOptionsService;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -29,13 +30,28 @@ public class CartResources {
     @Transactional
     @GetMapping
     public ResponseDto getAllCarts(Pageable pageable) {
-        return ResponseDto.of(cartService.findAll(pageable).map(CartDto::toDto), "Get all carts successfully");
+        Page<CartEntity> carts = cartService.findAll(pageable);
+        Page<CartDto> cartDtos = carts.map(CartDto::toDto);
+        carts.getContent().stream().forEach(cart -> {
+             OptionDto optionDto = OptionDto.toDto(this.optionsRepository.findById(cart.getOptionId()).orElse(null));
+             cartDtos.getContent().stream().forEach(cartDto -> {
+                 if (cartDto.getId() == cart.getId()) {
+                     cartDto.setOption(optionDto);
+                 }
+             });
+        });
+        return ResponseDto.of(cartDtos, "Get all carts successfully");
     }
 
     @Transactional
     @GetMapping("/{id}")
     public ResponseDto getCartById(@PathVariable("id") Long id) {
-        return ResponseDto.of(CartDto.toDto(cartService.findById(id)), "Get cart by id successfully");
+        CartEntity cartEntity = this.cartService.findById(id);
+        OptionDto optionDto = OptionDto.toDto(this.optionsRepository.findById(cartEntity.getOptionId()).get());
+        CartDto cartDto = CartDto.toDto(cartEntity);
+        cartDto.setOption(optionDto);
+
+        return ResponseDto.of(cartDto, "Get cart by id successfully");
     }
 
     @Transactional
