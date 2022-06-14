0694aa5a13fb7e84812f8c31758b1f727d52e19a
vuivehieu <75554300+vuivehieu@users.noreply.github.com>
2022-06-14 21:46:34 +0700

    Create Payment + Get Payment Detail
    abcd

---
+++ src/main/java/com/utils/VnPayUtils.java
@@ -0,0 +1,79 @@
+package com.utils;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class VnPayUtils {
+    public static String vnp_Version = "2.1.0";
+    public static String vnp_Command = "2.1.0";
+    public static String vnp_TmnCode = "BJ9ZILWP";
+
+    public static String vnp_HashSecret = "HEMVEOBMAWSXSMKKUTEDHPFWEPXSTVRY";
+
+    public static String vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+    public static String vnp_ResUrl = "http://localhost:8081/transaction/payment";
+    public static String vnp_BankCode = "NCB";
+    public static String vnp_CurrCode = "VND";
+    //    public static String vnp_IpAddr = "0:0:0:0:0:0:0:1";
+    public static String vnp_Locale = "vn";
+    public static String vnp_ReturnUrl = "";
+    public static String hmacSHA512(final String key, final String data) {
+        try {
+
+            if (key == null || data == null) {
+                throw new NullPointerException();
+            }
+            final Mac hmac512 = Mac.getInstance("HmacSHA512");
+            byte[] hmacKeyBytes = key.getBytes();
+            final SecretKeySpec secretKey = new SecretKeySpec(hmacKeyBytes, "HmacSHA512");
+            hmac512.init(secretKey);
+            byte[] dataBytes = data.getBytes(StandardCharsets.UTF_8);
+            byte[] result = hmac512.doFinal(dataBytes);
+            StringBuilder sb = new StringBuilder(2 * result.length);
+            for (byte b : result) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+            return sb.toString();
+
+        } catch (Exception ex) {
+            return "";
+        }
+    }
+    public static String getIpAddress(HttpServletRequest request) {
+        String ipAdress;
+        try {
+            ipAdress = request.getHeader("X-FORWARDED-FOR");
+            if (ipAdress == null) {
+                ipAdress = request.getRemoteAddr();
+            }
+        } catch (Exception e) {
+            ipAdress = "Invalid IP:" + e.getMessage();
+        }
+        return ipAdress;
+    }
+    public static String hashAllFields(Map fields) {
+        // create a list and sort it
+        List fieldNames = new ArrayList(fields.keySet());
+        Collections.sort(fieldNames);
+        // create a buffer for the md5 input and add the secure secret first
+        StringBuilder sb = new StringBuilder();
+
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) fields.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                sb.append(fieldName);
+                sb.append("=");
+                sb.append(fieldValue);
+            }
+            if (itr.hasNext()) {
+                sb.append("&");
+            }
+        }
+        return hmacSHA512(vnp_HashSecret, sb.toString());
+    }
+}
---
+++ src/main/java/com/webs/PaymentResources.java
@@ -0,0 +1,42 @@
+package com.webs;
+
+import com.dtos.ResponseDto;
+import com.models.PayModel;
+import com.services.VnPayService;
+import org.joda.time.DateTime;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@RestController
+@RequestMapping("/transaction")
+public class PaymentResources {
+    final VnPayService vnPayService;
+
+    public PaymentResources(VnPayService vnPayService) {
+        this.vnPayService = vnPayService;
+    }
+
+    @PostMapping("/checkout")
+    public ResponseDto sendPayRequest(@RequestBody @Valid PayModel payModel, HttpServletRequest request){
+        try {
+            return ResponseDto.of(vnPayService.PerformTransaction(payModel,request),"Send pay request");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @GetMapping("/payment")
+    public ResponseDto getPayResult(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+        System.out.println(DateTime.now());
+        return ResponseDto.of(vnPayService.getPaymentUrl(response,request),"Get Transaction Information");
+    }
+}
