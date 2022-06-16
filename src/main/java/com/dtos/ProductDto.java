f773ba32eec9f1f9f707b5c1b090227cbd098b67
Đinh Thế Vũ <thevu091193@gmail.com>
2022-06-16 15:46:53 +0700

    fix add product DTO
    go bo slug o class product DTO

--- src/main/java/com/dtos/ProductDto.java
+++ src/main/java/com/dtos/ProductDto.java
@@ -28,8 +28,6 @@ public class ProductDto {
     private Integer rating;
     private String avatar;
     private List<Object> attachFiles;
-    private String slug;
-
     private CategoryDto category;
     private List<ProductMetaDto> productMetas;
     private List<OptionDto> options;
@@ -47,7 +45,6 @@ public class ProductDto {
         productDto.setRating(entity.getRating());
         productDto.setAvatar(entity.getImage());
         productDto.setAttachFiles(entity.getAttachFiles() != null ? new JSONObject(entity.getAttachFiles()).getJSONArray("files").toList() : null);
-
         productDto.setCategory(entity.getCategory() == null ? null : CategoryDto.toDto(entity.getCategory(), false, false));
         productDto.setProductMetas(entity.getProductMetas().isEmpty() ? null : entity.getProductMetas().stream().map(ProductMetaDto::toDto).collect(Collectors.toList()));
         productDto.setOptions(entity.getOptions().isEmpty() ? null : entity.getOptions().stream().map(OptionDto::toDto).collect(Collectors.toList()));
