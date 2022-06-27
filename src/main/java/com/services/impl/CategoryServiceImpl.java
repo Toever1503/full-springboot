c0f34a845fc00961ca48484bd08b233a075bc744
Toever1503 <Haunv1503@gmail.com>
2022-06-27 16:49:33 +0700

    update category and industry

--- src/main/java/com/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/services/impl/CategoryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.services.impl;
 
+import com.dtos.ECategoryType;
 import com.entities.CategoryEntity;
 import com.entities.ProductEntity;
 import com.models.CategoryModel;
@@ -36,7 +37,7 @@ public class CategoryServiceImpl implements ICategoryService {
 
     @Override
     public List<CategoryEntity> findChildrenById(Long id) {
-        return this.categoryRepository.findAllByParentCategoryId(id);
+        return this.categoryRepository.findAllByParentCategoryIdAndType(id, ECategoryType.CATEGORY.name());
     }
 
     @Override
@@ -47,6 +48,37 @@ public class CategoryServiceImpl implements ICategoryService {
     @Override
     public Page<CategoryEntity> search(String q, Pageable page) {
         return this.categoryRepository.search(q, page);
+    }
+
+    @Override
+    public CategoryEntity addIndustry(CategoryModel model) {
+        CategoryEntity entity = CategoryModel.toEntity(model);
+        entity.setType(ECategoryType.INDUSTRY.name());
+        entity.setDeepLevel(0);
+        return this.categoryRepository.save(entity);
+    }
+
+    @Override
+    public CategoryEntity updateIndustry(CategoryModel model) {
+        CategoryEntity original = this.findById(model.getId());
+        CategoryEntity entity = CategoryModel.toEntity(model);
+        entity.setId(original.getId());
+        entity.setChildCategories(original.getChildCategories());
+        entity.setType(ECategoryType.INDUSTRY.name());
+        return this.categoryRepository.save(entity);
+    }
+
+    @Override
+    public CategoryEntity findOne(Specification<CategoryEntity> spec) {
+        return this.categoryRepository.findOne(spec).orElseThrow(() -> new RuntimeException("Category not found"));
+    }
+
+    @Override
+    public boolean deleteIndustryById(Long id) {
+        this.categoryRepository.deleteByIdAndType(id, ECategoryType.INDUSTRY.name());
+        this.categoryRepository.updateProductIndustry(id);
+        this.categoryRepository.updateCategoryIndustry(id);
+        return true;
     }
 
 
@@ -63,11 +95,13 @@ public class CategoryServiceImpl implements ICategoryService {
     @Override
     public CategoryEntity add(CategoryModel model) {
         CategoryEntity categoryEntity = CategoryModel.toEntity(model);
+        categoryEntity.setType(ECategoryType.CATEGORY.name());
         categoryEntity.setDeepLevel(0);
         if (this.categoryRepository.findBySlug(model.getSlug()).isPresent())
             throw new RuntimeException("Slug already existed!");
 
         this.saveParentCategory(categoryEntity, model.getParentId());
+        this.saveIndustry(categoryEntity, model.getIndustryId());
         return this.categoryRepository.save(categoryEntity);
     }
 
@@ -83,18 +117,29 @@ public class CategoryServiceImpl implements ICategoryService {
         if (checkedCategory != null)
             if (!checkedCategory.getId().equals(model.getId()))
                 throw new RuntimeException("Slug already existed!");
-
         CategoryEntity originCategory = this.findById(model.getId());
         originCategory.setCategoryName(model.getCategoryName());
         originCategory.setSlug(slug);
         originCategory.setDescription(model.getDescription());
         this.saveParentCategory(originCategory, model.getParentId());
+        this.saveIndustry(originCategory, model.getIndustryId());
         return this.categoryRepository.save(originCategory);
+    }
+
+    private void saveIndustry(CategoryEntity entity, Long industry) {
+        if (industry != null) { //check if parent id not null\
+            CategoryEntity parentCategory = this.findById(industry);
+            if (parentCategory.getType().equals(ECategoryType.CATEGORY.name()))
+                throw new RuntimeException("Category can't be industry");
+            entity.setIndustry(parentCategory);
+        }
     }
 
     private void saveParentCategory(CategoryEntity entity, Long parentId) {
         if (parentId != null) { //check if parent id not null\
             CategoryEntity parentCategory = this.findById(parentId);
+            if (parentCategory.getType().equals(ECategoryType.INDUSTRY.name()))
+                throw new RuntimeException("Industry can't be parent category");
             if (parentCategory.getDeepLevel() >= 3)
                 throw new RuntimeException("Parent category is too deep!");
             entity.setParentCategory(parentCategory);
@@ -104,8 +149,9 @@ public class CategoryServiceImpl implements ICategoryService {
 
     @Override
     public boolean deleteById(Long id) {
-        this.categoryRepository.deleteById(id);
+        this.categoryRepository.deleteByIdAndType(id, ECategoryType.CATEGORY.name());
         this.categoryRepository.updateCategoryParent(id);
+        this.categoryRepository.updateProductCategory(id);
         return true;
     }
 
