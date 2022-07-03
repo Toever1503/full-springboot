c7433694f65e14b6c848498d93a23c4a714c578c
Toever1503 <Haunv1503@gmail.com>
2022-07-04 01:08:15 +0700

    fix elasticsearch online connection by other server

--- src/main/java/com/config/elasticsearch/ElasticsearchConfiguration.java
+++ src/main/java/com/config/elasticsearch/ElasticsearchConfiguration.java
@@ -5,8 +5,13 @@ import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.apache.http.message.BasicHeader;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContexts;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -16,6 +21,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.http.HttpHeaders;
+
+import javax.net.ssl.SSLContext;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.config.elasticsearch.ERepositories")
@@ -35,26 +42,70 @@ public class ElasticsearchConfiguration {
     @Value("${elasticsearch.password}")
     private String password;
 
-    @Bean(destroyMethod = "close")
-    public RestHighLevelClient restClient() {
-
+//    @Bean(destroyMethod = "close")
+//    public RestHighLevelClient restClient() {
+//
 //        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
 //        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(userName, password));
-//        RestClientBuilder builder = RestClient.builder(new HttpHost(host, port, protocol))
-//                .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
-//                .setDefaultHeaders(compatibilityHeaders());
-//        return new RestHighLevelClient(builder);
-
-        RestClient httpClient = RestClient.builder(
-                new HttpHost(host, port, protocol)
-        ).build();
-        RestHighLevelClient hlrc = new RestHighLevelClientBuilder(httpClient)
-                .setApiCompatibilityMode(true)
-                .build();
-        return hlrc;
-    }
+////        RestClientBuilder builder = RestClient.builder(new HttpHost(host, port, protocol))
+////                .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
+////                .setDefaultHeaders(compatibilityHeaders());
+////        return new RestHighLevelClient(builder);
+//
+//        RestClient httpClient = RestClient.builder(
+//                new HttpHost(host, port, protocol)
+//        ).setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
+//                .build();
+//        RestHighLevelClient hlrc = new RestHighLevelClientBuilder(httpClient)
+//                .setApiCompatibilityMode(true)
+//                .build();
+//        return hlrc;
+//    }
 
 //    private Header[] compatibilityHeaders() {
 //        return new Header[]{new BasicHeader(HttpHeaders.ACCEPT, "application/vnd.elasticsearch+json;compatible-with=7"), new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/vnd.elasticsearch+json;compatible-with=7")};
 //    }
+
+
+    @Bean
+    public RestHighLevelClient restClient() throws Exception {
+        try {
+            final CredentialsProvider credentialsProvider =
+                    new BasicCredentialsProvider();
+            credentialsProvider.setCredentials(AuthScope.ANY,
+                    new UsernamePasswordCredentials(this.userName, this.password));
+
+            SSLContextBuilder sslBuilder = SSLContexts.custom()
+                    .loadTrustMaterial(null, (x509Certificates, s) -> true);
+            final SSLContext sslContext = sslBuilder.build();
+            RestClient restClient = RestClient
+                    .builder(new HttpHost(this.host, this.port, this.protocol))
+//port number is given as 443 since its https schema
+                    .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                        @Override
+                        public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                            return httpClientBuilder
+                                    .setSSLContext(sslContext)
+                                    .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                                    .setDefaultCredentialsProvider(credentialsProvider);
+                        }
+                    })
+                    .setRequestConfigCallback(new RestClientBuilder.RequestConfigCallback() {
+                        @Override
+                        public RequestConfig.Builder customizeRequestConfig(
+                                RequestConfig.Builder requestConfigBuilder) {
+                            return requestConfigBuilder.setConnectTimeout(5000)
+                                    .setSocketTimeout(120000);
+                        }
+                    }).build();
+
+            RestHighLevelClient client = new RestHighLevelClientBuilder(restClient)
+                    .setApiCompatibilityMode(true).build();
+            System.out.println("elasticsearch client created");
+            return client;
+        } catch (Exception e) {
+            System.out.println(e);
+            throw new Exception("Could not create an elasticsearch client!!");
+        }
+    }
 }
