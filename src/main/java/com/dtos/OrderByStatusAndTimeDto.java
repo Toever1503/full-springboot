265dde8f0b963d900f640c8705ad45b603776886
Đinh Thế Vũ <thevu091193@gmail.com>
2022-07-05 11:36:41 +0700

    API Thong  ke
    THONG KE
    + don hang
    + SO LUONG san pham
    + Doanh thu
    + nguoi dung moi
    THEO STATUS VA TIME

--- src/main/java/com/dtos/OrderByStatusAndTimeDto.java
+++ src/main/java/com/dtos/OrderByStatusAndTimeDto.java
@@ -10,6 +10,7 @@ import javax.persistence.NamedStoredProcedureQuery;
 import javax.persistence.ParameterMode;
 import javax.persistence.StoredProcedureParameter;
 import javax.persistence.Temporal;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
@@ -18,22 +19,22 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
-@NamedStoredProcedureQuery(name = "order_by_status_and_time", procedureName = "order_by_status_and_time", resultClasses = OrderByStatusAndTimeDto.class, parameters = {
-        @StoredProcedureParameter(name = "status_order", mode = ParameterMode.IN, type = String.class),
-        @StoredProcedureParameter(name = "time_from", mode = ParameterMode.IN, type = Date.class),
-        @StoredProcedureParameter(name = "time_to", mode = ParameterMode.IN, type = Date.class),
-        @StoredProcedureParameter(name = "hour_in_day", type = Integer.class, mode = ParameterMode.OUT),
-        @StoredProcedureParameter(name = "total_order", type = Integer.class, mode = ParameterMode.OUT),
-        @StoredProcedureParameter(name = "status_order", type = String.class, mode = ParameterMode.OUT),
-        @StoredProcedureParameter(name = "total_products", type = Integer.class, mode = ParameterMode.OUT),
-        @StoredProcedureParameter(name = "total_prices", type = Double.class, mode = ParameterMode.OUT),
-        @StoredProcedureParameter(name = "order_date", type = Date.class, mode = ParameterMode.OUT),
-})
+//@NamedStoredProcedureQuery(name = "findOrderAndPriceByTimeAndStatus", procedureName = "order_by_status_and_time", resultClasses = OrderByStatusAndTimeDto.class, parameters = {
+//        @StoredProcedureParameter(name = "status_order", mode = ParameterMode.IN, type = String.class),
+//        @StoredProcedureParameter(name = "time_from", mode = ParameterMode.IN, type = String.class),
+//        @StoredProcedureParameter(name = "time_to", mode = ParameterMode.IN, type = String.class),
+//        @StoredProcedureParameter(name = "hour_in_day", type = Integer.class, mode = ParameterMode.OUT),
+//        @StoredProcedureParameter(name = "total_order", type = Integer.class, mode = ParameterMode.OUT),
+//        @StoredProcedureParameter(name = "status_order", type = String.class, mode = ParameterMode.OUT),
+//        @StoredProcedureParameter(name = "total_products", type = Integer.class, mode = ParameterMode.OUT),
+//        @StoredProcedureParameter(name = "total_prices", type = Double.class, mode = ParameterMode.OUT),
+//        @StoredProcedureParameter(name = "order_date", type = Date.class, mode = ParameterMode.OUT),
+//})
 public class OrderByStatusAndTimeDto {
     private Integer hour_in_day;
     private Integer total_order;
     private String status_order;
     private Integer total_products;
     private Double total_prices;
-    private Date order_date;
+    private LocalDateTime order_date;
 }
