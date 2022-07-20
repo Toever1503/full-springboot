5dd34a4a26ccddf3479f09cb8da02fba059f82eb
Toever1503 <Haunv1503@gmail.com>
2022-07-20 13:21:21 +0700

    FIX IP FRONTEND

--- src/main/java/com/config/FrontendConfiguration.java
+++ src/main/java/com/config/FrontendConfiguration.java
@@ -1,7 +1,7 @@
 package com.config;
 
 public class FrontendConfiguration {
-    public static String FE_URL = "http://15.164.227.244/";
+    public static String FE_URL = "http://3.38.92.188/";
 
     public static String QUESTION_DETAIL_URL = FE_URL + "question?detail=";
     public static String ORDER_DETAIL_URL = FE_URL + "order-detail/";
