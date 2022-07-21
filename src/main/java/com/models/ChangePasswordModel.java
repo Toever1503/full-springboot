7b491856fbdb528cf5787ab8736d46cf882d89d9
Đinh Thế Vũ <thevu091193@gmail.com>
2022-07-21 11:10:49 +0700

    fix change password

--- src/main/java/com/models/ChangePasswordModel.java
+++ src/main/java/com/models/ChangePasswordModel.java
@@ -3,6 +3,7 @@ package com.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
@@ -14,10 +15,10 @@ import javax.validation.constraints.NotNull;
 public class ChangePasswordModel {
     @NotNull
     @NotBlank
-    @Min(4)
+    @Length(min = 4, max = 20)
     private String oldPassword;
     @NotNull
     @NotBlank
-    @Min(4)
+    @Length(min = 4, max = 20)
     private String newPassword;
 }
