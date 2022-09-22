14e397ad7e0004bf5c5ffbc528aba8ce495866c4
Toever1503 <Haunv1503@gmail.com>
2022-09-22 09:27:40 +0700

    update elastic server ip

--- src/main/java/com/config/FrontendConfiguration.java
+++ src/main/java/com/config/FrontendConfiguration.java
@@ -1,11 +1,11 @@
 package com.config;
 
 public class FrontendConfiguration {
-    public static String FE_URL = "http://3.38.92.188/";
+    public static String FE_URL = "http://13.124.169.217/";
 
     public static String QUESTION_DETAIL_URL = FE_URL + "question?detail=";
     public static String ORDER_DETAIL_URL = FE_URL + "order-detail/";
     public static String ADMIN_ORDER_DETAIL_URL = FE_URL + "manage-orders?detail=";
 
-    public static String ORDER_CANCEL_URL = FE_URL + "order-failed/";
+    public static  String ORDER_CANCEL_URL = FE_URL + "order-failed/";
 }
